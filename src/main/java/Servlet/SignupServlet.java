e356c4881099b466305341ad417d154056909193
RecklessSnips <lloyddonegan@gmail.com>
2023-03-30 16:57:26 -0400

    Add signup servlet

---
+++ src/main/java/Servlet/SignupServlet.java
@@ -0,0 +1,68 @@
+package Servlet;
+
+import Databases.AccountAccessor;
+import Databases.Database;
+import Entities.Account;
+import Entities.AccountCredential;
+import Entities.AccountType;
+import Entities.Person;
+import Utilities.Address;
+import Utilities.Pair;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(urlPatterns = "/signup")
+public class SignupServlet extends HttpServlet {
+    Database db;
+    AccountAccessor accountAcc;
+
+    @Override
+    public void init() throws ServletException {
+        db = new Database();
+        db.connect();
+        accountAcc = new AccountAccessor(db);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doPost(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("first-name");
+        String lastName = req.getParameter("last-name");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String phoneNumber = req.getParameter("phone-number");
+        String SSN = req.getParameter("ssn");
+        String address = req.getParameter("address");
+
+        if ( ! (username != null && SSN != null)){
+
+        }
+
+        AccountCredential accountCredential = new AccountCredential(0, username, password);
+        Account account = new Account(0, username, SSN, AccountType.Client);
+        Person person = new Person(firstName, lastName, address);
+
+
+
+        req.getRequestDispatcher("/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Destroyed");
+        db.disconnect();
+        super.destroy();
+    }
+
+}
